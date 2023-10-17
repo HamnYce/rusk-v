dfd273c8e0d8c676abff082a5de877e57a5b91e4
Jant <hamadboa@gmail.com>
2023-10-17 13:07:08 +0300

    change functions to struct

--- src/foo.rs
+++ src/foo.rs
@@ -1,36 +1,44 @@
-pub fn _add(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = rs1 + rs2;
+struct fungi {
+    rd: i32,
+    rs1: i32,
+    rs2: i32,
 }
 
-pub fn _sub(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = rs1 - rs2;
-}
+impl fungi {
+    pub fn add(&mut self) {
+        self.rd = self.rs1 + self.rs2;
+    }
 
-pub fn xor(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = rs1 ^ rs2;
-}
+    pub fn sub(&mut self) {
+        self.rd = self.rs1 - self.rs2;
+    }
 
-pub fn or(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = rs1 | rs2;
-}
+    pub fn xor(&mut self) {
+        self.rd = self.rs1 ^ self.rs2;
+    }
 
-pub fn and(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = rs1 & rs2;
-}
+    pub fn or(&mut self) {
+        self.rd = self.rs1 | self.rs2;
+    }
 
-pub fn sll(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = rs1 << rs2;
-}
+    pub fn and(&mut self) {
+        self.rd = self.rs1 & self.rs2;
+    }
 
-pub fn sra(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = rs1 >> rs2;
-}
+    pub fn sll(&mut self) {
+        self.rd = self.rs1 << self.rs2;
+    }
 
-//TODO: this needs testing
-pub fn srl(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = ((*rs1 as u32) >> rs2) as i32
-}
+    pub fn sra(&mut self) {
+        self.rd = self.rs1 >> self.rs2;
+    }
 
-pub fn slt(rd: &mut i32, rs1: &i32, rs2: &i32) {
-    *rd = if rs1 < rs2 { 1 } else { 0 }
+    //TODO: this needs testing
+    pub fn srl(&mut self) {
+        self.rd = ((self.rs1 as u32) >> self.rs2) as i32
+    }
+
+    pub fn slt(&mut self) {
+        self.rd = if self.rs1 < self.rs2 { 1 } else { 0 }
+    }
 }
